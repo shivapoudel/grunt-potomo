7c450280d3f6a18161cb380ddd8a7f50f6e25606
Shiva Poudel <dev.shivapoudel@gmail.com>
2014-10-21 21:55:13 +0545

    Some indendation guys :)

--- test/potomo_test.js
+++ test/potomo_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var grunt = require('grunt');
+var grunt = require( 'grunt' );
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -23,25 +23,28 @@ var grunt = require('grunt');
 */
 
 exports.potomo = {
+	
 	setUp: function( done ) {
 		// setup here if necessary
 		done();
 	},
+	
 	en_GB: function( test ) {
 		test.expect(1);
 
-		var actual = grunt.file.read( 'tmp/en_GB.mo' );
+		var actual   = grunt.file.read( 'tmp/en_GB.mo' );
 		var expected = grunt.file.read( 'test/expected/en_GB.mo' );
-		test.equal(actual, expected, 'should compile PO to MO with msgfmt.');
+		test.equal( actual, expected, 'should compile PO to MO with msgfmt.' );
 
 		test.done();
 	},
+	
 	ne_NP: function( test ) {
 		test.expect(1);
 
-		var actual = grunt.file.read( 'tmp/ne_NP.mo' );
+		var actual   = grunt.file.read( 'tmp/ne_NP.mo' );
 		var expected = grunt.file.read( 'test/expected/ne_NP.mo' );
-		test.equal(actual, expected, 'should compile PO to MO with msgfmt.');
+		test.equal( actual, expected, 'should compile PO to MO with msgfmt.' );
 
 		test.done();
 	}
