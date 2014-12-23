d07a63519f00dee00e805315225732c6052993b9
Christian Foellmann <foellmann@foe-services.de>
2014-12-23 21:15:10 +0100

    bug out un-gracefully on Windows

--- tasks/potomo.js
+++ tasks/potomo.js
@@ -18,11 +18,13 @@ module.exports = function( grunt ) {
 		});
 
 		// Return warning if not found msgfmt command
-		if ( ! shell.which( 'msgfmt' ) ) {
-			return grunt.fail.warn(
-				'\nYou need to have "GNU Gettext" installed and in your PATH for this task to work.' +
-				'More info: http://www.gnu.org/software/gettext\n'
-			);
+		if ( process.platform !== 'win32' ) {
+			if ( ! shell.which( 'msgfmt' ) ) {
+				return grunt.fail.warn(
+					'\nYou need to have "GNU Gettext" installed and in your PATH for this task to work.' +
+					'More info: http://www.gnu.org/software/gettext\n'
+				);
+			}
 		}
 
 		if ( this.files.length < 1 ) {
